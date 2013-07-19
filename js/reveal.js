70bb8f8a92dea01b1e7ea2de38e1aef091715c5a
Greg Palmer <gpalmer323@gmail.com>
2013-07-18 20:01:49 -0700

    whitespace

--- js/reveal.js
+++ js/reveal.js
@@ -1650,12 +1650,12 @@ var Reveal = (function(){
 	}
 
 	/**
-	 * Updates the background elements to reflect the current 
+	 * Updates the background elements to reflect the current
 	 * slide.
 	 */
 	function updateBackground() {
 
-		// Update the classes of all backgrounds to match the 
+		// Update the classes of all backgrounds to match the
 		// states of their slides (past/present/future)
 		toArray( dom.background.childNodes ).forEach( function( backgroundh, h ) {
 
